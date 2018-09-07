b08c7e7313452dcb8c904b496f68f14e41e3abed
Darren Liang <darren.liang88@myhunter.cuny.edu>
2018-09-07 13:34:51 -0400

    finished string-1 problems

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -75,5 +75,16 @@ def first_half(str):
 def without_end(str):
   return str[1:len(str)-1]
 
+def combo_string(a, b):
+  if len(a) > len(b):
+    return b + a + b
+  return a + b + a
+  
+def non_start(a, b):
+  return a[1:] + b[1:]
+
+def left2(str):
+  return str[2:] + str[:2]
+
 
 
