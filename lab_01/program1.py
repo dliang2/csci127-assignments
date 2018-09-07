2491022b7c77102ebf56b6293920e195e783050e
Darren Liang <darren.liang88@myhunter.cuny.edu>
2018-09-07 12:40:31 -0400

    did some string

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -47,4 +47,33 @@ def front_back(str):
 def front3(str):
   return str[:3] * 3
 
+# String 1
+
+def hello_name(name):
+  return "Hello " + name + "!"
+
+def make_abba(a, b):
+  return a + b + b + a
+  
+def make_tags(tag, word):
+  return "<" + tag + ">" + word + "</" + tag + ">"
+  
+def make_out_word(out, word):
+  return out[:len(out)/2] + word + out[len(out)/2:]
+
+def extra_end(str):
+  return str[-2:]*3
+
+def first_two(str):
+  if len(str) >= 2:
+    return str[:2]
+  return str
+
+def first_half(str):
+  return str[:len(str)/2]
+
+def without_end(str):
+  return str[1:len(str)-1]
+
+
 
