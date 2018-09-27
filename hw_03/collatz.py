942f83c6d5816956ef4d182881e8b4fa47a0c23d
Darren Liang <darren.liang88@myhunter.cuny.edu>
2018-09-27 14:15:13 -0400

    added comments

--- hw_03/collatz.py
+++ hw_03/collatz.py
@@ -1,16 +1,16 @@
 # Darren Liang and Stacey Li
 
 def collatz(n):
-    steps = 0
-    while n > 1:
-        if n % 2 == 0:
+    steps = 0 # placeholder to count amount of steps
+    while n != 1: # keep looping until n equal to 1
+        if n % 2 == 0: # divide by 2 if even
             n = n / 2
+            print(int(n)) # remove decimals
+            steps += 1 # add 1 step
+        else: # multiply by 3 and add 1 if odd
+            n = (3 * n) + 1
             print(int(n))
-            steps += 1
-        else:
-            n = 3 * n + 1
-            print(int(n))
-            steps += 1
+            steps += 1 
     return "Steps: " + str(steps)
 
 print(collatz(9))
--- hw_03/sqrt.py
+++ hw_03/sqrt.py
@@ -1,18 +1,17 @@
 # Darren Liang and Stacey Li
 
 def mysqrt(n):
-    i = 1
-    oldguess = 1
-    while i > 0:
-        newguess = ((oldguess + (n / oldguess)) / 2)
+    oldguess = 1 # random guess, can be any number except for 0
+    while 1 > 0: # infinite loop
+        newguess = ((oldguess + (n / oldguess)) / 2) # get closer to the answer
         print(n, oldguess)
-        if newguess == oldguess:
+        if newguess == oldguess: # break the infinite loop when we are satisfied with our answer
             break
-        oldguess = newguess
-    return newguess
+        oldguess = newguess # makes oldguess the latest previous term
+    return "Final Answer: " + str(newguess)
 
 
 i = 1
-while i <= 20:
-    print(i, mysqrt(i))
+while i <= 20: # print out the steps for the sqrt of numbers from 1 to 20
+    print(mysqrt(i))
     i += 1
