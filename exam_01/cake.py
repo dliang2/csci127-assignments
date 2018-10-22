6bfb7a3332884dfdefeca01823ab376e3dd51789
Darren Liang <darren.liang88@myhunter.cuny.edu>
2018-10-22 09:05:53 -0400

    edits

--- exam_01/cake.py
+++ exam_01/cake.py
@@ -4,6 +4,6 @@ def cake(A, B, u):
     invite_count = int(slice_count) # never round up
     return invite_count
     
-print(cake(5, 10, 1)) # expect 2
-print(cake(10, 5, 7)) # expect 3
-print(cake(7, 4, 9)) # expect 5
+print(cake(5, 10, 1)) # expect 2 | .5 units per slice, 1 cake, 2 people
+print(cake(10, 5, 7)) # expect 3 | 2 units per slice, 7 cakes, 3 people (leftovers)
+print(cake(7, 4, 9)) # expect 5 | 1.75 units per slice, 9 cakes, 5 people
