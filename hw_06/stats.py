84bdc65acca2dadb613891f6f29a48507561cd68
Darren Liang <darren.liang88@myhunter.cuny.edu>
2018-10-29 09:52:19 -0400

    pog

--- hw_06/stats.py
+++ hw_06/stats.py
@@ -5,6 +5,7 @@
 value 1 to 10
 1. find the largest (location of one of them)
 2. frequency of a specific value
+3. mode
 """
 import random
 
@@ -29,7 +30,7 @@ def frequency(value, l):
 def mode(l):
     test_mode = l[0] # initial mode is first number of the list
     max_value = l[index_largest(l)] # highest number of list
-    for i in range(1, max_value + 1): # go through list from 1 to highest number
+    for i in range(0, max_value + 1): # go through list from 1 to highest number
         if frequency(test_mode, l) < frequency(i + 1, l): # compare modes
             test_mode = i + 1 # if number has higher freq count, becomes mode
     mode = test_mode
@@ -37,10 +38,10 @@ def mode(l):
         
 def bucket_mode(l, max_value):
     tallies = []
-    for i in range(max_value):
-        tallies.append(0)
-    for item in l:
-        tallies[item] = tallies[item] + 1
+    for i in range(max_value): # create 10 buckets
+        tallies.append(0) # each bucket starts at 0 count
+    for item in l: # add a count to their respective tally bucket
+        tallies[item] = tallies[item] + 1 
     li = index_largest(tallies)
     return li
     
@@ -53,5 +54,5 @@ for i in range(1, l[index_largest(l)] + 1):
 print("Mode:", mode(l))
 print("Frequency of Mode:", frequency(mode(l), l))
 
-print("Mode:", bucket_mode(l, 100))
-print("Frequency of Mode:", frequency(bucket_mode(l, 100), l))
+print("Mode:", bucket_mode(l, l[index_largest(l)] + 1))
+print("Frequency of Mode:", frequency(bucket_mode(l, l[index_largest(l)] + 1), l))
