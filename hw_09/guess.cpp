7c6c0066b30ac76070e364d369337abfe0ed69bf
Darren Liang <darren.liang88@myhunter.cuny.edu>
2018-11-29 13:54:45 -0500

    numba 9

---
+++ hw_09/guess.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+  int response;
+  int num;
+  int guess = 45;
+  int upper = 99;
+  int lower = 0;
+  std::cout << "Input a Number 0-99: ";
+  std::cin >> num;
+
+  std::cout << "Is " << guess << " the number? Respond with -1, 0, or 1. ";
+  std::cin >> response;
+  
+  while (response != 0) {
+    if (response == 1) {
+      upper = guess;
+    }
+    else {
+      lower = guess;
+    }
+    guess = (lower + upper)/2;
+
+    std::cout << "Is " << guess << " the number? Respond with -1, 0, or 1. ";
+    std::cin >> response;
+
+  }
+  std::cout << "I got it!" << std::endl;
+  return 0;
+}
